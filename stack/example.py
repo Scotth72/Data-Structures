fccd5311db84221c2ff06561e6086a7de6c4feb7
Heath Scott <you@example.com>
2020-06-08 17:22:34 -0500

    Initial commit

--- stack/example.py
+++
@@ -1,9 +0,0 @@
-# Print out each element of the following array on a separate line:
-# ['Joe', '2', 'Ted', '4.98', '14', 'Sam', 'void *', '42', 'float', 'pointers', '5006']
-# You may use whatever programming language you'd like.
-# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
-
-arr = ['Joe', '2', 'Ted', '4.98', '14', 'Sam',
-       'void *', '42', 'float', 'pointers', '5006']
-for i in arr:
-    print(i)
